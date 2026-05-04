11aff0c96d7517f8a0e3168c4b465759bfed9557
Arundhati1008 <thakurarundhati08@gmail.com>
2026-05-04 18:57:21 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-5}")( f"{product:<15}" )(f"{status:<12} ")(f"{date}") 
-
-
-    def search_products(self):
-     keyword = input("Enter product name or category to search: ")
-     results = self.db.search_products(keyword)
-
-     if not results:
